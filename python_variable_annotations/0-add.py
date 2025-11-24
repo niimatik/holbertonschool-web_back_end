99da001af6c0b772bfe623f7a32e7d6ae757d221
niimatik <niimatik.gg@gmail.com>
2025-11-24 14:40:53 +0100

    fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,5 +5,8 @@ Module simple pour effectuer des opérations arithmétiques de base.
 
 
 def add(a: float, b: float) -> float:
+    """
+    Calcule la somme de deux nombres flottants ou entiers.
+    """
     sum = a + b
     return sum
