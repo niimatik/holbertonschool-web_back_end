5dbe6024e653f1fd2f1ee717985d8040938d5b18
niimatik <niimatik.gg@gmail.com>
2025-11-24 14:48:16 +0100

    add

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module simple contenant des fonctions de manipulation de chaînes de caractères.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatène deux chaînes de caractères.
+    """
+    concated = str1 + str2  # Concaténation simple des deux chaînes
+    return concated
