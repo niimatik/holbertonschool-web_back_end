75b6acdcd4dcd656b162bab76f6039d18195341e
niimatik <niimatik.gg@gmail.com>
2025-11-24 15:03:39 +0100

    add

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module fournissant une fonction simple de troncature de nombre flottant.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Retourne un numero en string.
+    """
+    a = int(n)
+    return a
