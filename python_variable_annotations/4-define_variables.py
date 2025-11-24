f182e1e89fb6ad8e2038c8770775e65aa13e65e7
niimatik <niimatik.gg@gmail.com>
2025-11-24 15:38:36 +0100

    add

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-Module contenant quelques variables simples utilisées comme exemples de types de base.
+Module contenant quelques variables simples utilisées
+comme exemples de types de base.
 """
 
 a = 1            # Entier représentant une valeur numérique simple.
 pi = 3.14        # Nombre flottant approximant la constante π.
-# Booléen indiquant la compréhension des annotations.
 i_understand_annotations = True
+# Booléen indiquant la compréhension des annotations.
 school = "Holberton"  # Chaîne représentant le nom d'une école.
 a: int
 pi: float
