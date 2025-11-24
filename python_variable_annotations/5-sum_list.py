1e8dbafdcc24bdacd2d6e192c825baffac59eb5d
niimatik <niimatik.gg@gmail.com>
2025-11-24 16:11:38 +0100

    add

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module fournissant une fonction qui calcule la somme des valeurs d'une liste.
+"""
+
+def sum_list(input_list: list) -> float:
+    """
+    Retourne la somme de tous les éléments numériques d'une liste.
+    """
+    a: float = 0.0
+    for f in input_list:
+        a = a + f
+    return a
