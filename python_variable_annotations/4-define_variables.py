6b11d6d4ee74e4eb9c56c9401c6ecbf60cc183b3
niimatik <niimatik.gg@gmail.com>
2025-11-24 15:36:55 +0100

    add

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -5,5 +5,10 @@ Module contenant quelques variables simples utilisées comme exemples de types d
 
 a = 1            # Entier représentant une valeur numérique simple.
 pi = 3.14        # Nombre flottant approximant la constante π.
-i_understand_annotations = True  # Booléen indiquant la compréhension des annotations.
+# Booléen indiquant la compréhension des annotations.
+i_understand_annotations = True
 school = "Holberton"  # Chaîne représentant le nom d'une école.
+a: int
+pi: float
+i_understand_annotations: bool
+school: str
