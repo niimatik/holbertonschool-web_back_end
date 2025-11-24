0fa3c70bce664d92826f920cbf1288c12ffbbb50
niimatik <niimatik.gg@gmail.com>
2025-11-24 15:00:07 +0100

    add

--- 2-floor.py
+++ 2-floor.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+Module fournissant une fonction simple de troncature de nombre flottant.
+"""
+
 
 def floor(n: float) -> int:
-    
+    """
+    Retourne la partie entière d'un nombre en le tronquant vers zéro.
+    """
+    a = int(n)
+    return a
