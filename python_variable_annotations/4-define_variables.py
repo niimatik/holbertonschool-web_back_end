68b942ba5e77b37067b3252ef09cecd8507942b6
niimatik <niimatik.gg@gmail.com>
2025-11-24 15:33:35 +0100

    add

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module contenant quelques variables simples utilisées comme exemples de types de base.
+"""
+
+
+a = 1
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
