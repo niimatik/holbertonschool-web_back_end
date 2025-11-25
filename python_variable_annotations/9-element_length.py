551a29a0fa36f4da8340bc14142fe7678cdadaea
niimatik <niimatik.gg@gmail.com>
2025-11-25 10:36:23 +0100

    add

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module fournissant une fonction qui associe
+chaque élément d’un iterable à sa longueur.
+"""
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """
+    Retourne une liste de tuples contenant chaque élément et sa longueur.
+    """
+    return [(i, len(i)) for i in lst]
