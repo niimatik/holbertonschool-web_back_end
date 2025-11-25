f40b799821dfa26d3bcee54832e1a6ad7d3902d8
niimatik <niimatik.gg@gmail.com>
2025-11-25 09:30:22 +0100

    add

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import typing
 """
 Module fournissant une fonction qui calcule la somme des valeurs d'une liste.
 """
 
+
+import typing
 
 def sum_list(input_list: typing.List[float]) -> float:
     """
