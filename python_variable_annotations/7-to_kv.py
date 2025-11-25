906c95d2914d99722e5dd189ebf7f24e68ec5194
niimatik <niimatik.gg@gmail.com>
2025-11-25 09:52:55 +0100

    add

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ une clé et une valeur numérique en tuple.
 import typing
 
 
-def to_kv(k: str,v: typing.Union[int,float]) -> typing.Tuple[str, float]:
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
     """
     Retourne un tuple dont la première valeur
     est la chaîne k et la seconde est basée sur v.
