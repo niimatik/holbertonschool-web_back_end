f4fbe31c78865ed80884da54e4ce5c974480d8c1
niimatik <niimatik.gg@gmail.com>
2025-11-25 09:51:53 +0100

    add

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -13,5 +13,5 @@ def to_kv(k: str,v: typing.Union[int,float]) -> typing.Tuple[str, float]:
     Retourne un tuple dont la première valeur
     est la chaîne k et la seconde est basée sur v.
     """
-    a: tuple = (k, sqrt(v))
+    a: tuple = (k, v ** 2)
     return a
