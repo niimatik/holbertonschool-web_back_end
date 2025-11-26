6efac4264c229f04317b3f6a841f04578ebc5f5c
niimatik <niimatik.gg@gmail.com>
2025-11-27 00:11:47 +0100

    add

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that defines a function to create an asyncio Task from the
+wait_random coroutine."""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Creates and returns an asyncio Task for the wait_random coroutine.
+    """
+    return asyncio.create_task(wait_random(max_delay))
