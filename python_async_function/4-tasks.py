25ee2900b119bbe192b0d96c2656785547429baa
niimatik <niimatik.gg@gmail.com>
2025-11-27 00:12:53 +0100

    add

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Asynchronous task scheduler that waits for a series of delays."""
+
+import asyncio
+import typing
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """
+    Execute multiple asynchronous tasks that wait for a random delay.
+    """
+    delays: typing.List[float] = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
