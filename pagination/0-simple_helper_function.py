9cee1ef4c7f45f9e45727c36bb1ac57e1af642eb
niimatik <niimatik.gg@gmail.com>
2025-12-01 09:59:17 +0100

    fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -10,9 +10,7 @@ def index_range(page: int, page_size: int) -> tuple:
     Retourne une structure contenant l’index de début et
     l’index de fin pour une page donnée.
     """
-    index = []
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    index.append(start_index)
-    index.append(end_index)
+    index = (start_index, end_index)
     return index
