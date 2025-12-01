bf35c5471f841a8545de268826a4fb50eb08660e
niimatik <niimatik.gg@gmail.com>
2025-12-01 09:52:09 +0100

    add

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module fournissant une fonction qui calcule un intervalle d’index
+basé sur une page et une taille de page.
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Retourne une structure contenant l’index de début et
+    l’index de fin pour une page donnée.
+    """
+    index = []
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    index.append(start_index, end_index)
+    return index
