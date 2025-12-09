66e0f2b34399cbd1573c03fd321fe8cc14f0883e
niimatik <niimatik.gg@gmail.com>
2025-12-09 15:30:41 +0100

    add

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newarray = []
+  for (const idx in array) {
+    newarray.push(appendString + value);
+  }
+
+  return array;
+}
