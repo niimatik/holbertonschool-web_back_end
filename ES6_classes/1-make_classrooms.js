73a35eb16be235b90373fadeac50caac363ca75a
niimatik <niimatik.gg@gmail.com>
2025-12-10 10:39:55 +0100

    add

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from "./0-classroom.js";
+
+export default function initializeRooms() {
+  const arraySize = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ]
+
+  return arraySize;
+}
