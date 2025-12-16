614f57edbb09aaf9e5222bdf931ad86914706088
niimatik <niimatik.gg@gmail.com>
2025-12-16 10:19:53 +0100

    add

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+export default function updateStudentGradeByCity(studentlist, city, newgrades) {
+  const locationarray = studentlist.filter(element => element.location === city);
+  const updatearray = locationarray.map(element => {
+    const gradefound = newgrades.find(item => item.studentID === element.id)
+    let grade
+    if (gradefound) {
+      grade = gradefound.grade
+    }
+    else {
+      grade = "N/A"
+    };
+    return {
+      ...studentID,
+      grade: grade
+    }
+  });
+  return updatearray
+}
