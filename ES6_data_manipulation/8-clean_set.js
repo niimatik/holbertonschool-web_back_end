dee6f4cd1eaac11777946ebb42b9f254ef91c6aa
niimatik <niimatik.gg@gmail.com>
2025-12-17 15:42:05 +0100

    add

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (startString === "" || typeof startString !== String) {
-    return "";
-  };
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
   const result = [];
-  for (const i of set) {
-    if (i.startWith(startString) && typeof i === "string") {
-      const cleanvalue = i.slice(startString.length);
-      result.push(cleanvalue);
-    };
-  };
-  return result.join("-");
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+  return result.join('-');
 }
