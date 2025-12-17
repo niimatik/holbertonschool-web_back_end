59cfa4487c9e2a645d6dcc76ca616d520c0754d5
niimatik <niimatik.gg@gmail.com>
2025-12-17 15:52:38 +0100

    add

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
   const groceries = new Map([
-    ["Apples", 10]
-    ["Tomatoes", 10]
-    ["Pasta", 1]
-    ["Rice", 1]
+    ["Apples", 10],
+    ["Tomatoes", 10],
+    ["Pasta", 1],
+    ["Rice", 1],
     ["Banana", 5]
   ]);
   return groceries;
