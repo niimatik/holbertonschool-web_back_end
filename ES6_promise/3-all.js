e88a55f04a45fe807ae5db1b8188cfaeb6d4b7f8
niimatik <niimatik.gg@gmail.com>
2025-12-18 23:49:25 +0100

    add

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import {uploadPhoto, createUser} from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return promise.all([uploadPhoto(), createUser()]) .then ((values) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
   })
-  .catch (() => {
-    console.log('Signup system offline');
-  })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
