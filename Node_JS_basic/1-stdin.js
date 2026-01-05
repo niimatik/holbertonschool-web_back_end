048664bd3d09f350743ce92f441d2105dd6d0ed7
niimatik <niimatik.gg@gmail.com>
2026-01-05 14:24:31 +0100

    fix: corrected the syntax to match eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
