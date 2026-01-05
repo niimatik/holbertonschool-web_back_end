3144a9375ae18f5dbece43ab3eefb5c68c5a1a32
niimatik <niimatik.gg@gmail.com>
2026-01-05 14:13:31 +0100

    fix: syntax requiered

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name? ')
+process.stdout.write('Welcome to Holberton School, what is your name?')
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data}`);
 });
