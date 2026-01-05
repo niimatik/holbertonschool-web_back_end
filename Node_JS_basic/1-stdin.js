1707d14ba567aab0f6bc76f6dca9c594e6260b51
niimatik <niimatik.gg@gmail.com>
2026-01-05 14:12:07 +0100

    add: completed the task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+process.stdout.write('Welcome to Holberton School, what is your name? ')
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data}`);
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
