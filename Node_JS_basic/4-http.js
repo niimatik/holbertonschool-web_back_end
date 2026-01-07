61bdb84c43e48a327db3d6955b87d27662403c85
niimatik <niimatik.gg@gmail.com>
2026-01-07 10:14:21 +0100

    fix: indentation

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,14 @@
 const http = require('http');
+
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
+
 const PORT = 1245;
+
 app.listen(PORT, 'localhost', () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
 module.exports = app;
