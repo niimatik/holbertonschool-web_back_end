f3987e366f253810853cec1a125b627aeeb1bc24
niimatik <niimatik.gg@gmail.com>
2026-01-07 10:12:02 +0100

    add: completed task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+const PORT = 1245;
+app.listen(PORT, 'localhost', () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+module.exports = app;
